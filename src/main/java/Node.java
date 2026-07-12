f22bf2b5a6e9d4293be2b22190b5e3fca69f0476
Jefferson Stanley Pereira Sousa <jeffersonstanley8@gmail.com>
2026-07-12 16:16:30 -0300

    Implementação da classe Node

---
+++ src/main/java/Node.java
@@ -0,0 +1,69 @@
+public class Node<K extends Comparable<K>, V> {
+
+    private int key;
+    private int value;
+    private Color color;
+    private Node<K, V> parent;
+    private Node<K, V> left;
+    private Node<K, V> right;
+
+    public Node(int key, int value) {
+        this.key = key;
+        this.value = value;
+        this.color = Color.RED;
+    }
+
+    public int getKey() {
+        return key;
+    }
+
+    public void setKey(int key) {
+        this.key = key;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    public Node<K, V> getParent() {
+        return parent;
+    }
+
+    public void setParent(Node<K, V> parent) {
+        this.parent = parent;
+    }
+
+    public Node<K, V> getLeft() {
+        return left;
+    }
+
+    public void setLeft(Node<K, V> left) {
+        this.left = left;
+    }
+
+    public Node<K, V> getRight() {
+        return right;
+    }
+
+    public void setRight(Node<K, V> right) {
+        this.right = right;
+    }
+
+}
+
+enum Color {
+    RED,
+    BLACK
+}
